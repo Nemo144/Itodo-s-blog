401cd3a5d3d9ecf17516042d321df589c791ab0b
Nnamdi Okeke <onnamdialex@gmail.com>
2022-02-09 16:30:51 +0100

    :art: complete the post the details page.

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -45,9 +45,7 @@ const PostCard = ({ post }: any) => {
               d="M10 19l-7-7m0 0l7-7m-7 7h18"
             />
           </svg>
-          <span className="">
-            {moment(post.createdAt).format('MMM DD, YYYY')}
-          </span>
+          <span>{moment(post.createdAt).format('MMM DD, YYYY')}</span>
         </div>
       </div>
       <p className="mb-8 px-4 text-center text-lg font-normal text-gray-700 lg:px-20">
--- components/PostDetail.tsx
+++ components/PostDetail.tsx
@@ -1,9 +1,113 @@
 import React from 'react'
+import moment from 'moment'
 
-const PostDetail = () => {
+const PostDetail = ({ post }: any) => {
+  const getContentFragment = (index: any, text: any, obj: any, type: any) => {
+    let modifiedText = text
+
+    if (obj) {
+      if (obj.bold) {
+        modifiedText = <b key={index}>{text}</b>
+      }
+
+      if (obj.italic) {
+        modifiedText = <em key={index}>{text}</em>
+      }
+
+      if (obj.underline) {
+        modifiedText = <u key={index}>{text}</u>
+      }
+    }
+
+    switch (type) {
+      case 'heading-three':
+        return (
+          <h3 key={index} className="mb-4 text-xl font-semibold">
+            {modifiedText.map((item: any, i: any) => (
+              <React.Fragment key={i}>{item}</React.Fragment>
+            ))}
+          </h3>
+        )
+      case 'paragraph':
+        return (
+          <p key={index} className="mb-8">
+            {modifiedText.map((item: any, i: any) => (
+              <React.Fragment key={i}>{item}</React.Fragment>
+            ))}
+          </p>
+        )
+      case 'heading-four':
+        return (
+          <h4 key={index} className="text-md mb-4 font-semibold">
+            {modifiedText.map((item: any, i: any) => (
+              <React.Fragment key={i}>{item}</React.Fragment>
+            ))}
+          </h4>
+        )
+      case 'image':
+        return (
+          <img
+            key={index}
+            alt={obj.title}
+            height={obj.height}
+            width={obj.width}
+            src={obj.src}
+          />
+        )
+      default:
+        return modifiedText
+    }
+  }
   return (
-    <div>
-      <h1>PostDetail</h1>
+    <div className="mb-8 rounded-lg bg-white pb-12 shadow-lg lg:p-8">
+      <div className="relative mb-6 overflow-hidden shadow-md">
+        <img
+          src={post.featuredImage.url}
+          alt={post.title}
+          className="h-full w-full rounded-t-lg object-top"
+        />
+      </div>
+      <div className="px-4 lg:p-0">
+        <div className="mb-8 flex w-full items-center">
+          <div className="mb-4 mr-8 flex w-full items-center  lg:mb-0 lg:w-auto">
+            <img
+              alt={post.author.name}
+              height="30px"
+              width="30px"
+              className="rounded-full align-middle"
+              src={post.author.photo?.url}
+            />
+            <p className="ml-2 inline align-middle text-lg text-gray-700">
+              {post.author.name}
+            </p>
+          </div>
+          <div className="mb-5 font-medium text-gray-700">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6 w-full text-white"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M10 19l-7-7m0 0l7-7m-7 7h18"
+              />
+            </svg>
+            <span>{moment(post.createdAt).format('MMM DD, YYYY')}</span>
+          </div>
+        </div>
+        <h1 className="mb-8 text-3xl font-semibold">{post.title}</h1>
+        {console.log(post.content.raw)}
+        {post.content.raw.children.map((typeObj: any, index: any) => {
+          const children = typeObj.children.map((item: any, itemIndex: any) =>
+            getContentFragment(itemIndex, item.text, item, index)
+          )
+          return getContentFragment(index, children, typeObj, typeObj.type)
+        })}
+      </div>
     </div>
   )
 }
